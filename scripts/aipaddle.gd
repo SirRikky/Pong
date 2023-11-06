77667d52aec7e27ee7fa35b7e7e7c0a95f43c970
SirRikky <94730503+SirRikky@users.noreply.github.com>
2023-11-06 16:51:22 +1100

    adds ai for paddle

--- scripts/aipaddle.gd
+++ scripts/aipaddle.gd
@@ -1,19 +1,12 @@
 extends RigidBody2D
 
+@export var paddle_speed = 35000
+@export var ball: Ball
 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-
-
-
+func _physics_process(delta):
+	
+	# Paddle moves towarde the ball
+	var direction = (ball.position - position).normalized()
+	linear_velocity.y = direction.y * paddle_speed * delta
 
